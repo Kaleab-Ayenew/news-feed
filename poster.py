d85faaf7a3301c2a85f571a9ca611420a788fdb7
Kaleab Ayenew <kaleab.ayenew2013@gmail.com>
2023-03-19 20:50:29 +0300

    Removed memory inefficient parts

--- poster.py
+++ poster.py
@@ -11,42 +11,35 @@ with open(r"site_list.json", "r") as list_file:
     sites = json.loads(list_file.read())
 
 
-def get_articles(url, site_name):
+def get_articles(url, site_name, token, ch_uname):
     paper = newspaper.build(url, memoize_articles=True)
-    content = []
+
     for article in paper.articles:
         article.download()
         article.parse()
         article.nlp()
-        content.append({'image': article.top_image, 'title': f"{article.title} | {site_name}",
-                       'text': article.summary.replace('\n', ' ')[:250]+"[...]", 'link': article.url})
-    return content
+        params = {'chat_id': ch_uname,
+                  'parse_mode': 'html',
+                  "reply_markup": {
+                      "inline_keyboard": [
+                          [{"text": "Read Full Article", "url": ""}, ]
+                      ]
+                  }
+                  }
 
+        bot_url = f'https://api.telegram.org/bot{token}/sendphoto'
 
-def post_to_telegram(url, site_name, token, ch_uname):
-    print(f"Getting {url}")
-    articles = get_articles(url, site_name)
-    params = {'chat_id': ch_uname,
-              'parse_mode': 'html',
-              "reply_markup": {
-                  "inline_keyboard": [
-                      [{"text": "Read Full Article", "url": ""}, ]
-                  ]
-              }
-              }
-    bot_url = f'https://api.telegram.org/bot{token}/sendphoto'
+        caption = '\n\n'.join(
+            [f"<b>{article.title} | {site_name}</b>", article.summary.replace('\n', ' ')[:250]+"[...]"])
 
-    for a in articles:
-        caption = '\n\n'.join([f"<b>{a.get('title')}</b>", a.get('text')])
-        params.update({'caption': caption, 'photo': a.get('image')})
-        params['reply_markup']['inline_keyboard'][0][0]['url'] = a.get(
-            'link')
-        # async with session.get(bot_url) as rsp:
-        #     print(f"[{rsp.status}] for link {url}")
-        #     js_rsp = await rsp.json()
-        print(a.get("link"))
+        params.update({'caption': caption, 'photo': article.top_image})
+
+        params['reply_markup']['inline_keyboard'][0][0]['url'] = article.url
+
         rsp = requests.post(bot_url, json=params)
+
         print(rsp.status_code)
+
         time.sleep(3)
 
 
@@ -59,11 +52,5 @@ def main_task():
         sys.exit(1)
 
     print("The main task has started execution")
-    proc_list = []
-    arg_list = []
-    # async with aiohttp.ClientSession(json_serialize=json.dumps) as session:
-    #     await asyncio.gather(*(post_to_telegram(
-    #         s[0], s[1], BOT_TOKEN, '@swenezt_test', session) for s in sites))
     for s in sites:
-        # arg_list.append((s[0], s[1], BOT_TOKEN, '@swenezt_test'))
-        post_to_telegram(s[0], s[1], BOT_TOKEN, '@swenezt_test')
+        get_articles(s[0], s[1], BOT_TOKEN, '@swenezt_test')
